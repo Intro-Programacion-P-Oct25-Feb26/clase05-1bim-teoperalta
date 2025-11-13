ed3c23bbc11d96d7903731d96d3815777265fe58
teoperalta <teo.peralta.2007@gmail.com>
2025-11-13 11:38:50 -0500

    placas

---
+++ ejemplos/Ejemplo4/src/ejemplo4/Ejemplo7.java
@@ -0,0 +1,41 @@
+/*
+ 
+ */
+package ejemplo4;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo7 {
+
+    public static void main(String[] args) {
+        // 
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        System.out.println("Ingrese la placa");
+        String placa = entrada.nextLine();
+
+        // L    0
+        // O    1
+        // J    2 
+        // A    3
+        // 
+        String inicial = placa.substring(0, 1); // se obtiene 
+        // el primer 
+        // caracter de la cadena
+        // Si el valor de ciudad es 
+        // "Loja" se obtiene "L"
+
+        if (((inicial.equals("G")) || (inicial.equals("g")))
+                || ((inicial.equals("P"))) || (inicial.equals("p")))  {
+            System.out.printf("%s\n", "acceso correcto");
+        } else {
+            System.out.printf("%s\n", "acceso incorrecto");
+        }
+
+    }
+}
